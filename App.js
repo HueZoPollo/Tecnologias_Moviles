8d798d5a695c3723665a8a1a61286e9b80caab2d
HéctorR <rhector6801@gmail.com>
2023-08-25 08:59:41 -0600

    Practica 2

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { StyledText } from './src/components/StyledText';
 
 // unidad1/practica1
 
@@ -8,24 +9,33 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Abre la terminal c:!</Text>
+      {/* <Text style={styles.texto}>Abre la terminal c:!</Text> */}
+      <StyledText text='Custom Styled Text' fontSize={40} fontWeight={'bold'} />
+      <StyledText text='Custom Styled Text 2' fontSize={20} color={'blue'} />
+      <TouchableOpacity style={styles.button} onPress={() => console.log('Hola mundo')}>
+        <StyledText text='Boton' fontSize={20} color={'red'} />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor:'#000'
+    backgroundColor:'#fff'
   },
   texto: {
     color: '#aff',
     fontSize: 30,
     fontWeight: 'normal',
+  },
+  button: {
+    borderWidth: 3,
+    padding: 5,
   }
   
 });
