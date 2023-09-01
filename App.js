bc366e1843d5a0625548a9426bd47009ae2e09ef
HéctorR <rhector6801@gmail.com>
2023-08-31 20:52:19 -0600

    Tarea 6

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text, ScrollView, FlatList } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import Login from "./src/screens/Login";
 import Person from "./src/components/Person/Person";
-
+import { facebook, google } from "./assets";
 
 const DATA = [
-  { id: "1", name: "Eder", lastName: "Rivera" },
-  { id: "2", name: "Pablo", lastName: "Juarez" },
-  { id: "3", name: "Ulises", lastName: "Rojas" },
-  { id: "4", name: "Arturo", lastName: "Martinez" },
-  { id: "6", name: "Diego", lastName: "Ruiz" },
-  { id: "7", name: "Diego", lastName: "Ruiz" },
-  { id: "8", name: "Diego", lastName: "Ruiz" },
-  { id: "9", name: "Diego", lastName: "Ruiz" },
-  { id: "10", name: "Diego", lastName: "Ruiz" },
-  { id: "11", name: "Diego", lastName: "Ruiz" },
-  { id: "12", name: "Diego", lastName: "Ruiz" },
+  { id: "1", name: "Eder", lastName: "Rivera", image: facebook },
+  { id: "2", name: "Pablo", lastName: "Juarez", image: google },
+  { id: "3", name: "Ulises", lastName: "Rojas", image: facebook },
+  { id: "4", name: "Arturo", lastName: "Martinez", image: google },
+  { id: "6", name: "Diego", lastName: "Ruiz", image: facebook },
+  { id: "7", name: "Alan", lastName: "Garcia", image: google },
+  { id: "8", name: "Gregory", lastName: "House", image: facebook },
+  { id: "9", name: "Barrita", lastName: "Licea", image: google },
+  { id: "10", name: "Angel", lastName: "Gallegos", image: facebook },
+  { id: "11", name: "Mario", lastName: "Gomez", image: google },
 ];
 
 export default function App() {
@@ -23,11 +22,15 @@ export default function App() {
     <View style={styles.container}>
       <FlatList
         data={DATA}
-        renderItem={({ item: { name, lastName }, index }) => 
-          <Person name={name} lastName={lastName} index={index} />
-        }
+        renderItem={({ item: { name, lastName, image }, index }) => (
+          <Person name={name} lastName={lastName} img={image} index={index} />
+        )}
         ItemSeparatorComponent={() => <View style={{ marginTop: 10 }} />}
-        keyExtractor={({id}) => id}
+        keyExtractor={({ id }) => id}
+        style={{ padding: 10 }}
+        contentContainerStyle={{ gap: 2 }}
+        horizontal={false}
+        numColumns={2}
       />
       {/* <Login /> */}
       <StatusBar style="auto" />
@@ -38,8 +41,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    // backgroundColor:'#fff'
   },
 });
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
-const Person = ({ name, lastName, index }) => {
+const Person = ({ name, lastName, index, img }) => {
   return (
     <View
-      style={{ borderWidth: 1, borderColor: index % 2 === 0 ? "red" : "blue" }}
+      style={{
+        borderRadius: 25,
+        backgroundColor:
+          index % 4 === 0
+            ? "#ff1270"
+            : index % 4 === 1
+            ? "#0bddfa"
+            : index % 4 === 2
+            ? "#ac44dd"
+            : "#f97",
+        margin: 10,
+        height: 200,
+        width: 165,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <Text style={{ fontSize: 35, padding: 10 }}>
-        {name} {lastName}
-      </Text>
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>{lastName}</Text>
+      <Image source={img} style={{ width: 100, height: 103 }} />
     </View>
-  )
-}
+  );
+};
 
-export default Person
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontFamily: "sans-serif",
+  },
+});
+
+export default Person;
