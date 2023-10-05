f61250bf595ab0bac7e2dcc56d84da2d12b37fa3
HéctorR <rhector6801@gmail.com>
2023-10-04 19:13:52 -0600

    Practica 9

--- Base/App.js
+++ Base/App.js
@@ -3,33 +3,32 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import Home from "./src/screens/Home";
-import Login from "./src/screens/Login";
-import { StyledText } from "./src/components/StyledText";
+// import { NavigationContainer } from "@react-navigation/native";
+// import Home from "./src/screens/Home";
+// import Login from "./src/screens/Login";
+// import { StyledText } from "./src/components/StyledText";
+import Card from "./src/components/Card";
+import AppContextProvider from "./src/components/Context/AppContext";
 
 const Stack = createStackNavigator();
 
+const person = {
+  id: 1,
+  name: "Pablo",
+  lastname: "Juarez",
+  age: 22,
+  location: "Morelia",
+  country: "Mexico",
+};
+
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              title: "My home",
-              headerStyle: {
-                backgroundColor: "white",
-              },
-            }}
-          />
-          <Stack.Screen name="Login" component={Login} />
-        </Stack.Navigator>
+    <AppContextProvider>
+      <View style={styles.container}>
+        <Card />
         <StatusBar style="auto" />
-      </NavigationContainer>
-    </SafeAreaView>
+      </View>
+    </AppContextProvider>
   );
 }
 
